a570da69709ef72611af6c5f8fff517a5266fffe
B M Al-amin Badhon <alamin.badhonn@gmail.com>
2023-05-22 16:30:00 +0600

    Port Agency full done

--- App.DAL/Models/TblPortAgency.cs
+++ App.DAL/Models/TblPortAgency.cs
@@ -16,6 +16,6 @@ namespace App.DAL.Models
         public DateTime? UpdatedDate { get; set; }
         public int? UpdatedBy { get; set; }
         [NotMapped]
-        public IFormFile PhotoUpload { get; set; }
+        public IFormFile? PhotoUpload { get; set; }
     }
 }
